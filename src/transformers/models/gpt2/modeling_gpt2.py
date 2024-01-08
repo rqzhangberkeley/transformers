6d5ae5e899f04a07254b7f3dd34fdf2f42efd3c7
Ruiqi Zhang <114196393+rqzhangberkeley@users.noreply.github.com>
2024-01-08 09:51:13 -0500

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -476,7 +476,9 @@ class GPT2PreTrainedModel(PreTrainedModel):
         #
         # Reference (Megatron-LM): https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/model/gpt_model.py
         for name, p in module.named_parameters():
-            if name == "c_proj.weight":
+            if name == "c_proj.weight": 
+                # this is only for the projection model.
+                # if we do not use the MLP layer, then this could be modified to self.config.n_layer without '2*'. 
                 # Special Scaled Initialization --> There are 2 Layer Norms per Transformer Block
                 p.data.normal_(mean=0.0, std=(self.config.initializer_range / math.sqrt(2 * self.config.n_layer)))
 
